8f45a142f212352facc40bec478542642d211b3f
Veronika <mainyvar@gmail.com>
2016-08-11 17:23:32 +0500

    add solution for prime task

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 /*generate PI up to decimal place.*/
 
-function Fibonacci () {
+function Fibonacci() {
     this.prev = 0;
     this.cur = 1;
     this.buf = 0;
@@ -9,63 +9,95 @@ function Fibonacci () {
 
     this.runInfinite = function () {
         var that = this;
-        setTimeout(function(){
+        setTimeout(function () {
             console.log(that.cur);
             that.buf = that.cur;
             that.cur = that.cur + that.prev;
             that.prev = that.buf;
-            $('#fibonacci').append('<p>'+that.cur+'</p>');
-            window.scrollTo(0,document.body.scrollHeight);
+            $('#fibonacci').append('<p>' + that.cur + '</p>');
+            window.scrollTo(0, document.body.scrollHeight);
             that.runInfinite();
-        },this.timer);
+        }, this.timer);
     };
 
-    this.runToNum = function(num) {
+    this.runToNum = function (num) {
         console.log(this.cur);
         this.buf = this.cur;
         this.cur = this.cur + this.prev;
         this.prev = this.buf;
-        $('#fibonacci').append('<p>'+this.cur+'</p>');
-        if(num > this.cur)
+        $('#fibonacci').append('<p>' + this.cur + '</p>');
+        if (num > this.cur)
             this.runToNum(num);
     }
 }
 
-var getNewPI = function(number){
+var getNewPI = function (number) {
     var pi = Math.PI;
     var piStr = pi.toString();
     return piStr.slice(0, piStr.lastIndexOf(number) + 1);
 };
 
-var getNewExp = function(number){
+var getNewExp = function (number) {
     var exp = Math.E;
     var expStr = exp.toString();
     return expStr.slice(0, expStr.lastIndexOf(number) + 1);
 };
 
-var getNumberSequenceFibonacci = function(num){
+var getNumberSequenceFibonacci = function (num) {
     var fib = new Fibonacci();
     //fib.runToNum(1000);
     fib.runInfinite();
 };
 
-var getNumberFibonacci = function(num){
+var getNumberFibonacci = function (num) {
     var prev = 0;
 
-    var fib = function(cur) {
+    var fib = function (cur) {
         prev = cur;
-        cur+=prev;
+        cur += prev;
         console.log(cur);
-        if(num > cur)
+        if (num > cur)
             fib(cur);
     };
     fib(1);
 };
 
 
-$(document).ready(function(){
-    console.log(getNewPI(1));
-    console.log(getNewExp(8));
-    getNumberSequenceFibonacci(8);
-    getNumberFibonacci(99);
+$(document).ready(function () {
+
+    var input_block = $('#input'),
+        result_block = $('#prime-result'),
+        text="";
+    var array = [];
+    $('#submit').on('click', function () {
+        var value = input_block.val();
+        array = [];
+        result_block.val("");
+
+        while (!(value % 2) && (value > 2)) {
+            array.push(2);
+            value = value / 2;
+        }
+        while (!(value % 3) && (value > 3)) {
+            array.push(3);
+            value = value / 3;
+        }
+        while (!(value % 5) && (value > 5)) {
+            array.push(5);
+            value = value / 5;
+        }
+        while (!(value % 7) && (value > 7)) {
+            array.push(7);
+            value = value / 7;
+        }
+        array.push(value);
+        for (var i = 0; i < array.length; i++) {
+            result_block.text(array[i]);
+            text += array[i].toString();
+            if(i != array.length-1) {
+                text += " x ";
+            }
+        }
+        result_block.text(text);
+    });
 });
