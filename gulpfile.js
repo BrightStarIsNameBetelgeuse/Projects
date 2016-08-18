2279c48f23415e9dcb1f5d4226a77a08f6c44c06
Veronika <mainyvar@gmail.com>
2016-08-18 12:04:40 +0500

    add gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+/**
+ * Created by Andromeda on 21.02.2016.
+ */
+var gulp = require('gulp'),
+    less = require('gulp-less'),
+    concat = require('gulp-concat'),
+    autoprefixer = require('gulp-autoprefixer'),
+    watch = require('gulp-watch')
+
+gulp.task('less', function(){
+    gulp.src(['less/*.less'])
+        .pipe(less())
+        .pipe(autoprefixer('last 2 version'))
+        .pipe(concat('main.css'))
+        .pipe(gulp.dest('css'));
+});
+
+gulp.task('watch', ['less'],function(){
+    gulp.watch('styles/less/*.less',['less']);
+});
