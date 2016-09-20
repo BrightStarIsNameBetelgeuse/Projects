367a3f819cc61fc47561cf5a79227a4679a430e2
Veronika <mainyvar@gmail.com>
2016-09-20 17:52:50 +0500

    add react components

---
+++ client/index.js
@@ -0,0 +1,8 @@
+/**
+ * Created by mainy on 20.09.2016.
+ */
+import React from 'react';
+import {render} from 'react-dom';
+import App from './components/App'
+
+render(<App />, document.getElementById('app'));
