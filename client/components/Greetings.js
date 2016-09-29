d4fd6f9bd7a2f3f38dee4d36999889fd01e08fee
Veronika <mainyvar@gmail.com>
2016-09-29 17:15:53 +0500

    add routing

--- client/components/Greetings.js
+++ client/components/Greetings.js
@@ -2,6 +2,10 @@ import React from 'react';
 
 export default () => {
     return(
+        <div>
         <h1>Hi!!!!</h1>
+        <p><a href="#">first</a></p>
+        <p><a href="#">second</a></p>
+            </div>
     );
 }
--- client/index.js
+++ client/index.js
@@ -2,7 +2,9 @@
  * Created by mainy on 20.09.2016.
  */
 import React from 'react';
-import {render} from 'react-dom';
+import {render} from 'react-dom'
+import {Router, browserHistory} from 'react-router';
+import routes from './routes';
 import App from './components/App'
 
-render(<App />, document.getElementById('app'));
+render(<Router history={browserHistory} routes={routes} />, document.getElementById('app'));
